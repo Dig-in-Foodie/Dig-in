50b100865f7f044b7b2689459827c6968a171bf4
davelard <davelard@cisco.com>
2023-06-26 16:37:12 -0400

    added info regarding db, and set up for server

---
+++ server/db/User.js
@@ -0,0 +1,12 @@
+const {Sequelize, sequelize} = require('./db')
+
+const User = sequelize.define('user',{
+    username: Sequelize.STRING,
+    password: Sequelize.STRING,
+    isAdmin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+    }
+});
+
+module.exports = {User};
