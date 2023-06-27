770dd1494d1c2a37f2dfab35606d9ccc220981b0
davelard <davelard@cisco.com>
2023-06-27 16:20:34 -0400

    added post register and login for authorization endpoints

--- server/index.js
+++ server/index.js
@@ -40,3 +40,70 @@ const setUser =(async (req,res,next)=>{
         next()
     }
 })
+
+//admin rights
+const admin = (req,res,next)=>{
+    if(req.user && req.user.isAdmin){
+        
+        next()
+    }else{
+        res.sendStatus(401)
+    }
+  }
+
+  //dashboard page once logged in
+
+  app.get('/dashboard',setUser,async(req,res,next)=>{
+    try{
+        const {token} = req.query
+       
+    }catch(error){
+        console.log(error)
+        next(error)
+    }
+    
+
+})
+//post register
+app.post("/register", async(req,res,next)=>{
+    try{
+        const {username, password}= req.body;
+        const hash = await bcrypt.hash(password, 10);
+        const user = await User.create({username,password:hash})
+        const token = jwt.sign({username, id:user.id, isAdmin: user.isAdmin}, JWT_SECRET)
+        
+        res.send({message:"foodie registered", token: token})
+        //add here res.json to connect with react
+      
+    }catch(error){
+        next(error)
+    }
+       
+  })
+
+  //post for login
+  app.post('/login', async(req,res,next)=>{
+    try{
+        const {username, password} = req.body
+        const foundUser = await User.findOne({where:{username}})
+        if(!foundUser){
+            res.sendStatus(401)
+        }else{
+            const isMatch = await bcrypt.compare(password, foundUser.password)
+            if (!isMatch){
+                res.sendStatus(401)
+            }else{
+              const token= jwt.sign({username, id:foundUser.id, isAdmin: foundUser.isAdmin},JWT_SECRET)
+        res.send({ message: 'Welcome to new bee', token: token})
+      
+      
+      
+            }
+          }
+          }catch(error){
+    next(error)
+  }
+})
+
+
+
